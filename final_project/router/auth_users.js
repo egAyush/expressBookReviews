c0e95db093a15b1fb86912c454062e4f2c555ee5
egAyush <ayushpandey3269@gmail.com>
2024-07-25 09:26:10 +0530

    task-8 completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,8 +56,26 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
+  const username=req.session.authorization && req.session.authorization.username;
+
+  const isbn=req.params.isbn;
+  const review=req.query.reviews;
+
+  if (!review) {
+    return res.status(400).json({ message: "Review details are missing" });
+  }
+
+  const book=books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  book.reviews=book.reviews || {};
+  book.reviews[username]=review;
+  return res.status(200).json({ message: "Review added/updated successfully" });
+  
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 module.exports.authenticated = regd_users;
