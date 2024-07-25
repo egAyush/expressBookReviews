5b29f30de41a31cf8a03343a0cfe224e72c7ec67
egAyush <ayushpandey3269@gmail.com>
2024-07-25 10:22:40 +0530

    task-9 completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -78,6 +78,22 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+
+  const username=req.session.authorization && req.session.authorization.username;
+
+  const isbn=req.params.isbn;
+
+  const book=books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  delete book.reviews[username];
+  return res.status(200).json({ message: "Review removed successfully" });
+
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
